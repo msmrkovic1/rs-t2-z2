110cf17eb4ab601765f27b7b207999c8184f9776
Mirza Smrkovic <msmrkovic1@etf.unsa.ba>
2019-03-18 12:49:20 +0100

    dodaj sumaCifara metodu

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -1,8 +1,28 @@
 package ba.unsa.etf.rs;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int n;
 
+        System.out.println("Enter a number: ");
+
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+
+        for (int i=1; i<=n; i++)
+        if(n % sumaCifara(i) == 0) System.out.print(n + ", ");
+
+    }
+
+    private static int sumaCifara(int n) {
+        int sum=0;
+        while(n>0) {
+            sum+=n%10;
+            n/=10;
+        }
+
+        return sum;
     }
 }
